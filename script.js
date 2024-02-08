d4e58e2d6e64e479c2a5c1e6432d88ef8d446b17
amitsingh4india <76247705+amitsingh4india@users.noreply.github.com>
2024-02-08 20:38:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 function fibonacci(num) {
 // your code here
 	
-    if (num === 0) return 0;
-    if (num === 1) return 1;
-
-    let prevPrev = 0;
-    let prev = 1;
-    let current = 1;
-
-    for (let i = 2; i < num; i++) {
-        current = prev + prevPrev;
-        prevPrev = prev;
-        prev = current;
-    }
-
-    return prevPrev;
+    if(num<0){
+		return "Invalid Number"
+	}
+	else if(num==1){
+		return 0
+	}
+	else if(num==2){
+		return 1
+	}
+	else{
+		 return fibonacci(num-1)+fibonacci(num-2)
+	}
 
 }
 
